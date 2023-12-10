9b6f0bbd4225139bfb837af63a03999d5737f526
arpitghura <ghurarpit110@gmail.com>
2023-12-10 23:47:46 +0530

    Added functionality to edit inplace and showing all appointment using a modal

---
+++ src/EditableCell.jsx
@@ -0,0 +1,76 @@
+import { TableCell } from "flowbite-react";
+import React, { useState } from "react";
+
+export const EditableCell = ({
+  value: initialValue,
+  isEditing,
+  updateItem, // This is a custom function that we supplied to our table instance
+  id: id,
+  inputName,
+  editingNodeId
+}) => {
+  // We need to keep and update the state of the cell normally
+  const [value, setValue] = useState(initialValue);
+  const [fieldName, setFieldName] = useState(null);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+    setFieldName(e.target.name);
+  };
+
+  // We'll only update the external data when the input is blurred
+  const onBlur = () => {
+    updateItem(id, value, fieldName);
+  };
+
+  // If the initialValue is changed external, sync it up with our state
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  /**
+   * Event handler to make a row editable.
+   * @param e
+   */
+  const setRowEditing = (e) => {
+    // TODO
+  };
+
+  let retObj = null;
+  if (isEditing && editingNodeId == id) {
+    switch (id) {
+      default:
+        retObj = (
+          <input
+            className="input-edit inline-block w-3/4 border border-gray-300 rounded p-2 leading-tight focus:outline-none focus:bg-white focus:border-blue-500  dark:bg-gray-800 dark:border-gray-700 dark:text-white"
+            name={inputName}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+        );
+        break;
+    }
+  } else {
+    switch (id) {
+      // case "action_btn":
+      //   retObj = (
+      //     <>
+      //       <button
+      //         className="btn btn-sm btn-info btn-sm-td"
+      //         onClick={setRowEditing}
+      //       >
+      //         {isEditing ? "Save" : "Edit"}
+      //       </button>
+      //     </>
+      //   );
+      //   break;
+      default:
+        retObj = <div>{value}</div>;
+        break;
+    }
+  }
+  return retObj;
+};
+
+export default EditableCell;
---
+++ src/InfoModal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const InfoModal = ( { setIsShowModal , userData}) => {
+  return (
+    <div className="fixed w-full h-full top-0 left-0 flex items-center justify-center">
+      <div className="absolute w-full h-full bg-gray-900 opacity-50"></div>
+
+      <div className="bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+        <div className="py-4 text-left px-6">
+          <div className="flex justify-between items-center pb-3">
+            <p className="text-2xl font-bold">User Details</p>
+          </div>
+          <div className="mb-2">
+            <p className="text-sm font-semibold text-gray-600">First Name:</p>
+            <p className="text-lg font-medium text-black">
+              {userData.firstName}
+            </p>
+          </div>
+          <div className="mb-2">
+            <p className="text-sm font-semibold text-gray-600">Last Name:</p>
+            <p className="text-lg font-medium text-black"></p>
+          </div>
+          <div className="mb-2">
+            <p className="text-sm font-semibold text-gray-600">Location:</p>
+            <p className="text-lg font-medium text-black"></p>
+          </div>
+          <div className="mb-2">
+            <p className="text-sm font-semibold text-gray-600">Appointments:</p>
+            <p className="text-lg font-medium text-black"></p>
+          </div>
+
+          <div className="mt-4 flex justify-end">
+            <button
+              className="px-4 bg-blue-500 py-2 ml-3 rounded-lg border-red-400 text-white hover:bg-blue-400"
+              onClick={() => setIsShowModal(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoModal;
